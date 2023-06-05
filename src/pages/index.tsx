c9b42d24204d403742bb1367058d49676b25bbb2
kennedygamarra <kennedy.gamarra@hypersoft.co>
2023-06-05 09:11:14 -0500

    feat: create userRouter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  const {data} = api.user.getAll.useQuery();
+
 
   return (
     <>
@@ -44,10 +46,11 @@ const Home: NextPage = () => {
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
             <AuthShowcase />
+            {data?.map((e) => (
+              <p className="text-white font-bold" key={e.id}>{e.name}</p>
+            )
+            )}
           </div>
         </div>
       </main>
@@ -60,16 +63,10 @@ export default Home;
 const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
