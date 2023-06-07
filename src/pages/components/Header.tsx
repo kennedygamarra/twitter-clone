744150fc72c1870cf78d11f67c0cff1a6dce8cb3
kennedygamarra <kennedygamarrad@gmail.com>
2023-06-07 14:19:02 -0500

    refactor: header style

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="fixed z-10 h-14 w-full align-middle  border-b border-white/10 px-4">
+    <header className="fixed z-10 h-14 w-[35rem] align-middle  border-b border-r border-white/10 px-4">
       <h1 className="bg-inherit pl-1 pt-3 text-2xl font-medium">Explore</h1>
     </header>
   );
