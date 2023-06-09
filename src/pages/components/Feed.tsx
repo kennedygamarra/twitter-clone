5ccd98acad64b4b798a81fbed29a98c43442dce7
kennedygamarra <kennedygamarrad@gmail.com>
2023-06-08 20:11:05 -0500

    feat: add styles to feed

--- src/pages/components/Feed.tsx
+++ src/pages/components/Feed.tsx
@@ -1,14 +1,15 @@
+import { api } from "~/utils/api";
 import Header from "./Header";
 import NewForm from "./NewForm";
-
+import { useSession } from "next-auth/react";
 
 const Feed = () => {
   return (
-    <div className="m-auto ml-52 flex w-full flex-col">
+    <div className="m-auto mr-0 ml-52 flex flex-col w-[35rem] border-r border-white/10 ">
       <Header />
-      <div className="mt-16 flex flex-col gap-4 px-4">
+      <div className="mt-16 flex flex-col gap-4">  
         <NewForm />
-        <div className="h-64 w-48 bg-red-300"></div>
+        <RecentTweets/>
         <div className="h-64 w-48 bg-red-300"></div>
         <div className="h-64 w-48 bg-red-300"></div>
         <div className="h-64 w-48 bg-red-300"></div>
@@ -16,6 +17,14 @@ const Feed = () => {
       </div>
     </div>
   );
+}
+
+function RecentTweets() {
+  const id = useSession().data?.user.id;
+
+  const tweets = api.tweet.getFeed.useQuery().data
+
+  return <InfiniteTweetList tweets={tweets}/>
 }
 
 export default Feed;
