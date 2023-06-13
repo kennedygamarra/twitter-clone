41be7c2eeb1b40930ec6a6dc7073538dfdb1890c
kennedygamarra <kennedygamarrad@gmail.com>
2023-06-12 23:26:56 -0500

    feat: like button styles

--- src/pages/components/InfiniteTweetList.tsx
+++ src/pages/components/InfiniteTweetList.tsx
@@ -1,6 +1,8 @@
 import InfiniteScroll from "react-infinite-scroll-component";
 import ProfileImage from "./ProfileImage";
 import Link from "next/link";
+import { VscHeartFilled, VscHeart } from "react-icons/vsc";
+import { useSession } from "next-auth/react";
 
 type Tweet = {
   id: string;
@@ -48,33 +50,65 @@ const InfiniteTweetList = ({
       </InfiniteScroll>
     </ul>
   );
-}
+};
 
-const dateFormatter = new Intl.DateTimeFormat(undefined, {dateStyle:"short"})
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "short",
+});
 
 function TweetPost({
   user,
   content,
   createdAt,
-  // likesCount,
-  // isLikedByMe,
+  likesCount,
+  isLikedByMe,
 }: Tweet) {
   return (
-    <div className="p-4 border-b border-white/10">
-      <div className="m-auto flex flex-row px-2 gap-4">
+    <div className="border-b border-white/10 p-4">
+      <header className="m-auto flex flex-row gap-4 px-2">
         <Link href={`/profiles/${user.id}`}>
-          <ProfileImage imageUrl={user.image}/>
+          <ProfileImage imageUrl={user.image} />
         </Link>
         <div className="flex flex-col">
           <div className="flex flex-row gap-4">
             <h1 className="font-semibold">{user.name}</h1>
-            <p className="font-light text-white/40">{dateFormatter.format(createdAt)}</p>
+            <p className="font-light text-white/40">
+              {dateFormatter.format(createdAt)}
+            </p>
           </div>
           <p className="font-light">{content}</p>
         </div>
-      </div>
+      </header>
+      <footer className="text-center align-middle items-center">
+        <LikeButton isLikedByMe={isLikedByMe} likesCount={likesCount} />
+      </footer>
     </div>
   );
+}
+
+type LikeButtonProps = {
+  isLikedByMe: boolean;
+  likesCount: number;
+};
+
+function LikeButton({ isLikedByMe, likesCount }: LikeButtonProps) {
+  const session = useSession();
+  const HeartIcon = isLikedByMe ? VscHeartFilled : VscHeart;
+
+    if (session.status !== "authenticated") {
+      return (
+        <button className="group flex flex-row gap-2">
+          <HeartIcon/>
+          <span>{likesCount}</span>
+        </button>
+      )
+    }
+    return (
+      <button className={`group flex flex-row gap-2 transition-colors ${isLikedByMe ? "text-pink-600" : "text-gray-600 hover:text-pink-600 focus-visible:text-pink-600"}`}>
+          <HeartIcon className={`transition-colors duration-200 ${isLikedByMe ? "fill-pink-600" : "fill-gray-600 group-hover:fill-pink-600 group-focus-visible:fill-pink-600"}`}/>
+          <span>{likesCount}</span>
+      </button>
+    )
 }
 
 export default InfiniteTweetList;
